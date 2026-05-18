2062f2b1101d2646a8a6e64c29853f6c6ae89131
krish-patel-simform <krish.patel@simformsolutions.com>
2026-05-18 14:47:18 +0530

    Implement basic introduction

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
-      
+      <h1>Hi, My name is Krish Patel</h1>
+      <p>
+        <b>This are my details:</b>
+      </p>
+      <p>
+        Github:
+        <a href="https://github.com/krish-patel-simform" target="_blank">
+          krish-patel-simform's Github
+        </a>
+      </p>
+      <p>
+        Email:
+        <a href="mailto:krish.patel@simformsolutions.com" target="_blank">
+          krish.patel@simformsolutions.com
+        </a>
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
